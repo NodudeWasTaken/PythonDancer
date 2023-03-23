14c9a02b4ac9d203bf850d78921b2297e1c11804
Nodude <>
2023-03-23 23:53:01 +0100

    plz work i beg u

--- extra-hooks/hook-librosa.py
+++ extra-hooks/hook-librosa.py
@@ -1,2 +1,5 @@
 from PyInstaller.utils.hooks import collect_data_files
 datas = collect_data_files("librosa")
+datas+= collect_data_files("librosa.core")
+datas+= collect_data_files("librosa.feature")
+datas+= collect_data_files("librosa.util")
