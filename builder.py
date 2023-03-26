64e153d4c51c32ef322b6630224dcf34cd486f0d
Nodude <>
2023-03-26 13:30:51 +0200

    Use urlopen

--- builder.py
+++ builder.py
@@ -1,4 +1,4 @@
-import urllib3
+from urllib.request import urlopen
 import io
 import shutil
 import os
@@ -6,9 +6,8 @@ import os
 import zipfile
 
 print("Download UPX")
-http = urllib3.PoolManager()
-with http.request("GET", "https://github.com/upx/upx/releases/download/v4.0.2/upx-4.0.2-win64.zip") as r:
-	with zipfile.ZipFile(io.BytesIO(r.data)) as z:
+with urlopen("https://github.com/upx/upx/releases/download/v4.0.2/upx-4.0.2-win64.zip") as r:
+	with zipfile.ZipFile(io.BytesIO(r)) as z:
 		for file in z.infolist():
 			if ("upx.exe" in file.filename):
 				with z.open(file.filename) as zf:
@@ -23,8 +22,8 @@ os.system("pip install -r requirements.txt --prefer-binary")
 os.system("pyinstaller kinter.spec")
 
 print("Download FFMpeg")
-with http.request("GET", "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip") as r:
-	with zipfile.ZipFile(io.BytesIO(r.data)) as z:
+with urlopen("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip") as r:
+	with zipfile.ZipFile(io.BytesIO(r)) as z:
 		for file in z.infolist():
 			if ("ffmpeg.exe" in file.filename):
 				with z.open(file.filename) as zf:
