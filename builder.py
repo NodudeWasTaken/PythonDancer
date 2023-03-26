5142cab16dc0c42227be3fb80457cd1117ec9c59
Nodude <>
2023-03-26 13:34:09 +0200

    AA

--- builder.py
+++ builder.py
@@ -1,4 +1,5 @@
-from urllib.request import urlopen
+os.system("pip install requests")
+import requests
 import io
 import shutil
 import os
@@ -6,8 +7,8 @@ import os
 import zipfile
 
 print("Download UPX")
-with urlopen("https://github.com/upx/upx/releases/download/v4.0.2/upx-4.0.2-win64.zip") as r:
-	with zipfile.ZipFile(io.BytesIO(r)) as z:
+with requests.get("https://github.com/upx/upx/releases/download/v4.0.2/upx-4.0.2-win64.zip") as r:
+	with zipfile.ZipFile(io.BytesIO(r.content)) as z:
 		for file in z.infolist():
 			if ("upx.exe" in file.filename):
 				with z.open(file.filename) as zf:
@@ -22,8 +23,8 @@ os.system("pip install -r requirements.txt --prefer-binary")
 os.system("pyinstaller kinter.spec")
 
 print("Download FFMpeg")
-with urlopen("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip") as r:
-	with zipfile.ZipFile(io.BytesIO(r)) as z:
+with requests.get("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip") as r:
+	with zipfile.ZipFile(io.BytesIO(r.content)) as z:
 		for file in z.infolist():
 			if ("ffmpeg.exe" in file.filename):
 				with z.open(file.filename) as zf:
