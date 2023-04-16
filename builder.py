df2d497cf574d75129e6395f2e69ab8933b5a01d
Nodude <>
2023-04-16 23:00:10 +0200

    Stop shipping with ffmpeg

--- builder.py
+++ builder.py
@@ -17,14 +17,4 @@ with requests.get("https://github.com/upx/upx/releases/download/v4.0.2/upx-4.0.2
 print("Building")
 os.system("pyinstaller qt.spec")
 
-print("Download FFMpeg")
-with requests.get("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip") as r:
-	with zipfile.ZipFile(io.BytesIO(r.content)) as z:
-		for file in z.infolist():
-			if ("ffmpeg.exe" in file.filename):
-				with z.open(file.filename) as zf:
-					with open("dist/ffmpeg.exe", "wb") as f:
-						shutil.copyfileobj(zf, f)
-						break
-
 print("Done!")
